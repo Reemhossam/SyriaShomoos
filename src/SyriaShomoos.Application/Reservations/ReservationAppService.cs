b918c05958ded2f66e8150d6dd2df27293e96bbe
Reem <Reem@DESKTOP-OQO8JHP>
2026-04-13 15:43:20 +0200

    add new services

---
+++ src/SyriaShomoos.Application/Reservations/ReservationAppService.cs
@@ -0,0 +1,119 @@
+﻿using SyriaShomoos.Entities;
+using SyriaShomoos.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace SyriaShomoos.Reservations
+{
+  public class ReservationAppService : ApplicationService
+  {
+
+    private readonly IRepository<Reservation, Guid> _repository;
+    private readonly IRepository<BranchSource, Guid> _branchRepository;
+
+    public ReservationAppService(IRepository<Reservation, Guid> repository, IRepository<BranchSource, Guid> branchRepository)
+    {
+      _repository = repository;
+      _branchRepository = branchRepository;
+    }
+
+    // CHECK-IN
+    public async Task CheckInAsync(CheckInReservationDto input)
+    {
+      var reservation = await _repository.FirstOrDefaultAsync(
+        x => x.ExternalIdentifier == input.ExternalIdentifier);
+
+      if (reservation == null)
+      {
+        reservation = new Reservation
+        {
+          ExternalIdentifier = input.ExternalIdentifier
+        };
+
+        await _repository.InsertAsync(reservation);
+      }
+
+      var branch = await _branchRepository.FirstOrDefaultAsync(
+          x => x.BranchCode == input.BranchCode);
+
+      if (branch == null)
+        throw new UserFriendlyException("Branch not found");
+
+      reservation.BranchSourceId = branch.Id;
+      reservation.RoomNumber = input.RoomNumber;
+      reservation.Status = ReservationStatus.CheckedIn;
+
+      reservation.MainGuest = new Guest
+      {
+        ReservationId = reservation.Id,
+        FullName = input.MainGuest.FullName,
+        IdentityNum = input.MainGuest.IdentityNum,
+        IdentityType = input.MainGuest.IdentityType,
+        Nationality = input.MainGuest.Nationality,
+        CheckInDate = input.MainGuest.CheckInDate
+      };
+
+      reservation.Escorts = input.Escorts?.Select(x => new GuestEscort
+      {
+        ReservationId = reservation.Id,
+        FullName = x.FullName,
+        IdentityNum = x.IdentityNum,
+        IdentityType = x.IdentityType,
+        Nationality = x.Nationality,
+        CheckInDate = x.CheckInDate
+      }).ToList() ?? new List<GuestEscort>();
+
+      await _repository.UpdateAsync(reservation);
+    }
+
+    public async Task CheckOutAsync(CheckOutReservationDto input)
+    {
+      var reservation = await _repository.FirstOrDefaultAsync(
+          x => x.ExternalIdentifier == input.ExternalIdentifier);
+
+      if (reservation == null)
+        throw new UserFriendlyException("Reservation not found");
+
+      if (reservation.Status != ReservationStatus.CheckedIn)
+        throw new UserFriendlyException("Reservation is not checked in");
+
+      reservation.Status = ReservationStatus.CheckedOut;
+
+      if (reservation.MainGuest != null)
+      {
+        reservation.MainGuest.CheckOutDate = input.CheckOutDate;
+        reservation.MainGuest.Rating = input.Rating;
+      }
+
+      foreach (var escort in reservation.Escorts)
+      {
+        escort.CheckOutDate = input.CheckOutDate;
+        escort.Rating = input.Rating;
+      }
+
+      await _repository.UpdateAsync(reservation);
+    }
+
+    public async Task CancelAsync(CancelReservationDto input)
+    {
+      var reservation = await _repository.FirstOrDefaultAsync(
+          x => x.ExternalIdentifier == input.ExternalIdentifier);
+
+      if (reservation == null)
+        throw new UserFriendlyException("Reservation not found");
+
+      if (reservation.Status == ReservationStatus.CheckedOut)
+        throw new UserFriendlyException("Cannot cancel after checkout");
+
+      reservation.Status = ReservationStatus.Cancelled;
+
+      await _repository.UpdateAsync(reservation);
+    }
+  }
+}
---
+++ src/SyriaShomoos.Application/Reservations/ReservationReadService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyriaShomoos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace SyriaShomoos.Reservations
+{
+  public class ReservationReadService : ApplicationService
+  {
+    private readonly IRepository<Reservation, Guid> _repository;
+
+    public ReservationReadService(IRepository<Reservation, Guid> repository)
+    {
+      _repository = repository;
+    }
+
+    public async Task<List<ReservationGridDto>> GetListAsync()
+    {
+      var query = await _repository.WithDetailsAsync(
+          x => x.MainGuest,
+          x => x.Escorts,
+          x => x.BranchSource);
+
+      return await query.Select(x => new ReservationGridDto
+      {
+        ExternalIdentifier = x.ExternalIdentifier,
+        FullName = x.MainGuest.FullName,
+        IdentityNum = x.MainGuest.IdentityNum,
+        RoomNumber = x.RoomNumber,
+        CheckInDate = x.MainGuest.CheckInDate,
+        CheckOutDate = x.MainGuest.CheckOutDate,
+        EscortsCount = x.Escorts.Count,
+        Status = x.Status.ToString(),
+        BranchCode = x.BranchSource.BranchCode
+      }).ToListAsync();
+    }
+  }
+}
