2e42afcc339db86b094caeb9f8489ff27d0da126
Reem <Reem@DESKTOP-OQO8JHP>
2026-04-20 12:28:32 +0200

    update filter dto

--- src/SyriaShomoos.Application.Contracts/Reservations/ReservationFilterDto.cs
+++ src/SyriaShomoos.Application.Contracts/Reservations/ReservationFilterDto.cs
@@ -8,11 +8,11 @@ namespace SyriaShomoos.Reservations
 {
   public class ReservationFilterDto
   {
-    public string GuestName { get; set; }
-    public string Property { get; set; }
-    public string IdentityType { get; set; }
-    public string IdentityNumber { get; set; }
-    public string UnitNumber { get; set; }
+    public string? GuestName { get; set; }
+    public string? Property { get; set; }
+    public string? IdentityType { get; set; }
+    public string? IdentityNumber { get; set; }
+    public string? UnitNumber { get; set; }
     public DateTime? FromDate { get; set; }
     public DateTime? ToDate { get; set; }
   }
