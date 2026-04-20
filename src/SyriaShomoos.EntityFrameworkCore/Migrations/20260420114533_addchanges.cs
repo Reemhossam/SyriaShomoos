56eccbc164b474bf647ec18f89ce0181dfd28f42
Reem <Reem@DESKTOP-OQO8JHP>
2026-04-20 13:51:02 +0200

    fix database errors

---
+++ src/SyriaShomoos.EntityFrameworkCore/Migrations/20260420114533_addchanges.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyriaShomoos.Migrations
+{
+    /// <inheritdoc />
+    public partial class addchanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Guests",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DateOfBirth",
+                table: "Guests",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
